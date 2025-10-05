283dc64e736340b2e5928f822783a56ec9a5c04b
Itxaka <itxaka@kairos.io>
2025-10-05 16:27:25 +0200

    Test
    Signed-off-by: Itxaka <itxaka@kairos.io>

---
+++ tests/acceptance.go
@@ -0,0 +1,205 @@
+package hadron_test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
+	. "github.com/spectrocloud/peg/matcher"
+)
+
+var stateContains = func(vm VM, query string, expected ...string) {
+	or := []types.GomegaMatcher{}
+	for _, e := range expected {
+		or = append(or, ContainSubstring(e))
+	}
+	out, err := vm.Sudo(fmt.Sprintf("kairos-agent state get %s", query))
+	ExpectWithOffset(1, err).ToNot(HaveOccurred())
+	ExpectWithOffset(1, strings.ToLower(out)).To(Or(or...))
+}
+
+var _ = Describe("kairos autoinstall test", Label("acceptance"), func() {
+	var vm VM
+	var datasource string
+
+	BeforeEach(func() {
+		datasource = CreateDatasource("assets/autoinstall.yaml")
+		Expect(os.Setenv("DATASOURCE", datasource)).ToNot(HaveOccurred())
+		_, vm = startVM()
+		vm.EventuallyConnects(1200)
+	})
+
+	AfterEach(func() {
+		if CurrentSpecReport().Failed() {
+			gatherLogs(vm)
+			serial, _ := os.ReadFile(filepath.Join(vm.StateDir, "serial.log"))
+			_ = os.MkdirAll("logs", os.ModePerm|os.ModeDir)
+			_ = os.WriteFile(filepath.Join("logs", "serial.log"), serial, os.ModePerm)
+			fmt.Println(string(serial))
+		}
+
+		err := vm.Destroy(nil)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(os.Unsetenv("DATASOURCE")).ToNot(HaveOccurred())
+		Expect(os.Remove(datasource)).ToNot(HaveOccurred())
+	})
+
+	Context("reboots and passes functional tests", func() {
+		BeforeEach(func() {
+			expectDefaultService(vm)
+			expectStartedInstallation(vm)
+			expectRebootedToActive(vm)
+		})
+
+		It("passes checks", func() {
+			By("checking grubenv file", func() {
+				out, err := vm.Sudo("cat /oem/grubenv")
+				Expect(err).ToNot(HaveOccurred(), out)
+				Expect(out).To(ContainSubstring("foobarzz"))
+			})
+
+			By("checking custom cmdline", func() {
+				out, err := vm.Sudo("cat /proc/cmdline")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("foobarzz"))
+			})
+
+			By("checking the use of dracut immutable module", func() {
+				out, err := vm.Sudo("cat /proc/cmdline")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("cos-img/filename="))
+			})
+
+			By("checking Auto assessment", func() {
+				// Auto assessment was installed
+				out, _ := vm.Sudo("cat /run/initramfs/cos-state/grubcustom")
+				Expect(out).To(ContainSubstring("bootfile_loc"))
+
+				out, _ = vm.Sudo("cat /run/initramfs/cos-state/grub_boot_assessment")
+				Expect(out).To(ContainSubstring("boot_assessment_blk"))
+
+				cmdline, _ := vm.Sudo("cat /proc/cmdline")
+				Expect(cmdline).To(ContainSubstring("rd.emergency=reboot rd.shell=0"))
+				Expect(cmdline).To(ContainSubstring("panic=5"))
+				Expect(cmdline).To(ContainSubstring("rd.shell=0"))
+			})
+
+			By("checking writeable tmp", func() {
+				_, err := vm.Sudo("echo 'foo' > /tmp/bar")
+				Expect(err).ToNot(HaveOccurred())
+
+				out, err := vm.Sudo("sudo cat /tmp/bar")
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(out).To(ContainSubstring("foo"))
+			})
+
+			By("checking bpf mount", func() {
+				Eventually(func() string {
+					out, _ := vm.Sudo("mount")
+					return out
+				}, 5*time.Minute, 1*time.Second).Should(
+					Or(
+						ContainSubstring("bpf"),
+					))
+			})
+
+			By("checking correct permissions", func() {
+				out, err := vm.Sudo(`stat -c "%a" /oem`)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("770"))
+
+				out, err = vm.Sudo(`stat -c "%a" /usr/local/cloud-config`)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("770"))
+			})
+
+			By("checking grubmenu", func() {
+				// Statereset is now part of the default grub.cfg
+				out, err := vm.Sudo("cat /etc/cos/grub.cfg")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("--id cos"))
+				Expect(out).To(ContainSubstring("--id fallback"))
+				Expect(out).To(ContainSubstring("--id recovery"))
+				Expect(out).To(ContainSubstring("--id statereset"))
+				// Now this one you can override with a custom grubmenu but by default we ship the remote recovery on it
+				out, err = vm.Sudo("cat /run/initramfs/cos-state/grubmenu")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("remoterecovery"))
+			})
+
+			By("checking additional mount specified, with no dir in rootfs", func() {
+				out, err := vm.Sudo("mount")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("/var/lib/longhorn"))
+			})
+
+			By("checking rootfs shared mount", func() {
+				out, err := vm.Sudo(`cat /proc/1/mountinfo | grep ' / / '`)
+				Expect(err).ToNot(HaveOccurred(), out)
+				Expect(out).To(ContainSubstring("shared"))
+			})
+
+			By("checking that it doesn't has grub data into the cloud config", func() {
+				out, err := vm.Sudo(`cat /oem/90_custom.yaml`)
+				Expect(err).ToNot(HaveOccurred(), out)
+				Expect(out).ToNot(ContainSubstring("vga_text"))
+				Expect(out).ToNot(ContainSubstring("videotest"))
+			})
+
+			By("checking that networking is functional", func() {
+				out, err := vm.Sudo(`curl google.it`)
+				Expect(err).ToNot(HaveOccurred(), out)
+				Expect(out).To(ContainSubstring("Moved"))
+			})
+
+			By("checking corresponding state", func() {
+				out, err := vm.Sudo("kairos-agent state")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("boot: active_boot"))
+				currentVersion, err := vm.Sudo(getVersionCmd)
+				Expect(err).ToNot(HaveOccurred(), currentVersion)
+
+				stateAssertVM(vm, "oem.mounted", "true")
+				stateAssertVM(vm, "oem.found", "true")
+				stateAssertVM(vm, "persistent.mounted", "true")
+				stateAssertVM(vm, "state.mounted", "true")
+				stateAssertVM(vm, "oem.type", "ext4")
+				stateAssertVM(vm, "persistent.type", "ext4")
+				stateAssertVM(vm, "state.type", "ext4")
+				stateAssertVM(vm, "oem.mount_point", "/oem")
+				stateAssertVM(vm, "persistent.mount_point", "/usr/local")
+				stateAssertVM(vm, "persistent.name", "/dev/vda")
+				stateAssertVM(vm, "state.mount_point", "/run/initramfs/cos-state")
+				stateAssertVM(vm, "oem.read_only", "false")
+				stateAssertVM(vm, "persistent.read_only", "false")
+				stateAssertVM(vm, "state.read_only", "true")
+				stateAssertVM(vm, "kairos.version", strings.ReplaceAll(strings.ReplaceAll(currentVersion, "\r", ""), "\n", ""))
+				stateContains(vm, "system.os.name", "alpine", "opensuse", "ubuntu", "debian")
+				stateContains(vm, "kairos.flavor", "alpine", "opensuse", "ubuntu", "debian")
+			})
+
+			By("Checking install/recovery services do not exist", func() {
+				if !isFlavor(vm, "alpine") {
+					for _, service := range []string{"kairos-interactive", "kairos-recovery"} {
+						By(fmt.Sprintf("Checking that service %s does nto exist", service), func() {})
+						Eventually(func() string {
+							out, _ := vm.Sudo(fmt.Sprintf("systemctl status %s", service))
+							return out
+						}, 3*time.Minute, 2*time.Second).Should(
+							And(
+								ContainSubstring(fmt.Sprintf("Unit %s.service could not be found", service)),
+							),
+						)
+					}
+				}
+			})
+		})
+	})
+})
---
+++ tests/tests_suite_test.go
@@ -0,0 +1,470 @@
+package hadron_test
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/diskfs/go-diskfs"
+	"github.com/diskfs/go-diskfs/disk"
+	"github.com/diskfs/go-diskfs/filesystem"
+	"github.com/diskfs/go-diskfs/filesystem/iso9660"
+	"github.com/google/uuid"
+	"github.com/kairos-io/go-nodepair"
+	qr "github.com/kairos-io/go-nodepair/qrcode"
+	"github.com/mudler/edgevpn/pkg/node"
+	process "github.com/mudler/go-processmanager"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/spectrocloud/peg/matcher"
+	"github.com/spectrocloud/peg/pkg/machine"
+	"github.com/spectrocloud/peg/pkg/machine/types"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "kairos Test Suite")
+}
+
+var getVersionCmd = ". /etc/kairos-release; [ ! -z \"$KAIROS_VERSION\" ] && echo $KAIROS_VERSION"
+var getVersionCmdOsRelease = ". /etc/os-release; [ ! -z \"$KAIROS_VERSION\" ] && echo $KAIROS_VERSION"
+
+// CreateDatasource creates a datasource iso from a given user-data file
+// And returns the path to the datasource iso
+// Its the caller's responsibility to remove the datasource iso afterwards
+func CreateDatasource(userDataFile string) string {
+	ds, err := os.MkdirTemp("", "datasource-*")
+	Expect(err).ToNot(HaveOccurred())
+	diskImg := path.Join(ds, "datasource.iso")
+	var diskSize int64 = 1 * 1024 * 1024 // 1 MB
+	mydisk, err := diskfs.Create(diskImg, diskSize, diskfs.SectorSizeDefault)
+	Expect(err).ToNot(HaveOccurred())
+	mydisk.LogicalBlocksize = 2048
+	fspec := disk.FilesystemSpec{Partition: 0, FSType: filesystem.TypeISO9660, VolumeLabel: "cidata"}
+	fs, err := mydisk.CreateFilesystem(fspec)
+	Expect(err).ToNot(HaveOccurred())
+	rw, err := fs.OpenFile("user-data", os.O_CREATE|os.O_RDWR)
+	Expect(err).ToNot(HaveOccurred())
+	content, err := os.ReadFile(userDataFile)
+	_, err = rw.Write(content)
+	Expect(rw.Close()).ToNot(HaveOccurred())
+	Expect(err).ToNot(HaveOccurred())
+	rw, err = fs.OpenFile("meta-data", os.O_CREATE|os.O_RDWR)
+	Expect(err).ToNot(HaveOccurred())
+	_, err = rw.Write([]byte(""))
+	Expect(rw.Close()).ToNot(HaveOccurred())
+	Expect(err).ToNot(HaveOccurred())
+	iso, ok := fs.(*iso9660.FileSystem)
+	Expect(ok).To(BeTrue())
+	err = iso.Finalize(iso9660.FinalizeOptions{RockRidge: true, VolumeIdentifier: "cidata"})
+	Expect(err).ToNot(HaveOccurred())
+	return diskImg
+}
+
+// https://gist.github.com/sevkin/96bdae9274465b2d09191384f86ef39d
+// GetFreePort asks the kernel for a free open port that is ready to use.
+func getFreePort() (port int, err error) {
+	var a *net.TCPAddr
+	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
+		var l *net.TCPListener
+		if l, err = net.ListenTCP("tcp", a); err == nil {
+			defer l.Close()
+			return l.Addr().(*net.TCPAddr).Port, nil
+		}
+	}
+	return
+}
+
+func user() string {
+	u := os.Getenv("SSH_USER")
+	if u == "" {
+		u = "kairos"
+	}
+	return u
+}
+
+func pass() string {
+	p := os.Getenv("SSH_PASS")
+	if p == "" {
+		p = "kairos"
+	}
+
+	return p
+}
+
+func gatherLogs(vm VM) {
+	// Use kairos-agent logs command to collect logs
+	vm.Sudo("kairos-agent logs --output /run/kairos-logs.tar.gz")
+
+	// Collect additional system information not covered by kairos-agent logs
+	vm.Sudo("cat /oem/* > /run/oem.yaml")
+	vm.Sudo("cat /etc/resolv.conf > /run/resolv.conf")
+	vm.Sudo("k3s kubectl get pods -A -o json > /run/pods.json")
+	vm.Sudo("k3s kubectl get events -A -o json > /run/events.json")
+	vm.Sudo("cat /proc/cmdline > /run/cmdline")
+	vm.Sudo("chmod 777 /run/events.json")
+
+	vm.Sudo("df -h > /run/disk")
+	vm.Sudo("mount > /run/mounts")
+	vm.Sudo("blkid > /run/blkid")
+	vm.Sudo("dmesg > /run/dmesg.log")
+
+	// Collect Kubernetes logs
+	vm.Scp("assets/kubernetes_logs.sh", "/tmp/logs.sh", "0770")
+	vm.Sudo("sh /tmp/logs.sh > /run/kube_logs")
+
+	vm.GatherAllLogs(
+		[]string{
+			"edgevpn@kairos",
+		},
+		[]string{
+			"/var/log/edgevpn.log",
+			"/run/pods.json",
+			"/run/disk",
+			"/run/mounts",
+			"/run/blkid",
+			"/run/events.json",
+			"/run/kube_logs",
+			"/run/cmdline",
+			"/run/oem.yaml",
+			"/run/resolv.conf",
+			"/run/dmesg.log",
+			"/tmp/ovmf_debug.log",
+			"/run/kairos-logs.tar.gz",
+		})
+}
+
+func startVM() (context.Context, VM) {
+	stateDir, err := os.MkdirTemp("", "")
+	Expect(err).ToNot(HaveOccurred())
+	fmt.Printf("State dir: %s\n", stateDir)
+
+	opts := defaultVMOpts(stateDir)
+
+	m, err := machine.New(opts...)
+	Expect(err).ToNot(HaveOccurred())
+
+	vm := NewVM(m, stateDir)
+
+	ctx, err := vm.Start(context.Background())
+	Expect(err).ToNot(HaveOccurred())
+
+	return ctx, vm
+}
+
+func isFlavor(vm VM, flavor string) bool {
+	out, err := vm.Sudo(fmt.Sprintf("cat /etc/os-release | grep ID=%s", flavor))
+	return err == nil && out != ""
+}
+
+func expectDefaultService(vm VM) {
+	By("checking if default service is active in live cd mode", func() {
+		if isFlavor(vm, "alpine") {
+			out, err := vm.Sudo("rc-status")
+			Expect(err).ToNot(HaveOccurred(), out)
+			Expect(out).Should(ContainSubstring("kairos-agent"))
+		} else {
+			// This is also run in the upgrade latest, so we need to check for both kairos-installer and kairos in case the service name changed
+			Eventually(func() string {
+				out, _ := vm.Sudo("systemctl status kairos-installer || systemctl status kairos")
+				return out
+			}, 3*time.Minute, 2*time.Second).Should(
+				Or(
+					ContainSubstring("loaded (/etc/systemd/system/kairos-installer.service; enabled;"),
+					ContainSubstring("loaded (/etc/systemd/system/kairos.service; enabled;"),
+				))
+		}
+	})
+}
+
+func expectStartedInstallation(vm VM) {
+	By("checking that installation has started", func() {
+		Eventually(func() string {
+			out, _ := vm.Sudo("ps aux")
+			return out
+		}, 30*time.Minute, 1*time.Second).Should(ContainSubstring("/usr/bin/kairos-agent install"))
+	})
+}
+
+func expectRebootedToActive(vm VM) {
+	By("checking that vm has rebooted to 'active'", func() {
+		Eventually(func() string {
+			out, _ := vm.Sudo("kairos-agent state boot")
+			return out
+		}, 40*time.Minute, 10*time.Second).Should(
+			Or(
+				ContainSubstring("active_boot"),
+			))
+	})
+}
+
+func expectSecureBootEnabled(vm VM) {
+	// Check for secureboot before install, based on firmware env var
+	// if we set, then the test suite will load the secureboot firmware
+	secureboot := os.Getenv("FIRMWARE")
+
+	if secureboot != "" {
+		By("checking that secureboot is enabled", func() {
+			out, _ := vm.Sudo("dmesg | grep -i secure")
+			Expect(out).To(ContainSubstring("Secure boot enabled"))
+		})
+	}
+}
+
+// return the PID of the swtpm (to be killed later) and the state directory
+func emulateTPM(stateDir string) {
+	t := path.Join(stateDir, "tpm")
+	err := os.MkdirAll(t, os.ModePerm)
+	Expect(err).ToNot(HaveOccurred())
+
+	cmd := exec.Command("swtpm",
+		"socket",
+		"--tpmstate", fmt.Sprintf("dir=%s", t),
+		"--ctrl", fmt.Sprintf("type=unixio,path=%s/swtpm-sock", t),
+		"--tpm2", "--log", "level=20")
+	err = cmd.Start()
+	Expect(err).ToNot(HaveOccurred())
+
+	err = os.WriteFile(path.Join(t, "pid"), []byte(strconv.Itoa(cmd.Process.Pid)), 0744)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+var kubectl = func(vm VM, s string) (string, error) {
+	return vm.Sudo("k3s kubectl " + s)
+}
+
+// Generates a valid token for provider tests
+func generateToken() string {
+	l := int(^uint(0) >> 1)
+	return node.GenerateNewConnectionData(l).Base64()
+}
+
+// register registers a node with a qrfile
+func register(loglevel, qrfile, configFile, device string, reboot bool) error {
+	b, _ := os.ReadFile(configFile)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if qrfile != "" {
+		fileInfo, err := os.Stat(qrfile)
+		if err != nil {
+			return err
+		}
+		if fileInfo.IsDir() {
+			return fmt.Errorf("cannot register with a directory, please pass a file") //nolint:revive // This is a message printed to the user.
+		}
+
+		if !isReadable(qrfile) {
+			return fmt.Errorf("cannot register with a file that is not readable") //nolint:revive // This is a message printed to the user.
+		}
+	}
+	// dmesg -D to suppress tty ev
+	fmt.Println("Sending registration payload, please wait")
+
+	config := map[string]string{
+		"device": device,
+		"cc":     string(b),
+	}
+
+	if reboot {
+		config["reboot"] = "true"
+	}
+
+	err := nodepair.Send(
+		ctx,
+		config,
+		nodepair.WithReader(qr.Reader),
+		nodepair.WithToken(qrfile),
+		nodepair.WithLogLevel(loglevel),
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Payload sent, installation will start on the machine briefly")
+	return nil
+}
+
+func isReadable(fileName string) bool {
+	file, err := os.Open(fileName)
+	if err != nil {
+		if os.IsPermission(err) {
+			return false
+		}
+	}
+	file.Close()
+	return true
+}
+
+func defaultVMOpts(stateDir string) []types.MachineOption {
+	opts := defaultVMOptsNoDrives(stateDir)
+
+	driveSize := os.Getenv("DRIVE_SIZE")
+	if driveSize == "" {
+		driveSize = "25000"
+	}
+
+	opts = append(opts, types.WithDriveSize(driveSize))
+
+	return opts
+}
+
+func defaultVMOptsNoDrives(stateDir string) []types.MachineOption {
+	var err error
+
+	if os.Getenv("ISO") == "" && os.Getenv("CREATE_VM") == "true" {
+		fmt.Println("ISO missing")
+		os.Exit(1)
+	}
+
+	var sshPort, spicePort int
+
+	vmName := uuid.New().String()
+
+	// Always setup a tpm emulator
+	emulateTPM(stateDir)
+
+	sshPort, err = getFreePort()
+	Expect(err).ToNot(HaveOccurred())
+	fmt.Printf("Using ssh port: %d\n", sshPort)
+
+	memory := os.Getenv("MEMORY")
+	if memory == "" {
+		memory = "2096"
+	}
+	cpus := os.Getenv("CPUS")
+	if cpus == "" {
+		cpus = "2"
+	}
+
+	opts := []types.MachineOption{
+		types.QEMUEngine,
+		types.WithISO(os.Getenv("ISO")),
+		types.WithMemory(memory),
+		types.WithCPU(cpus),
+		types.WithSSHPort(strconv.Itoa(sshPort)),
+		types.WithID(vmName),
+		types.WithSSHUser(user()),
+		types.WithSSHPass(pass()),
+		types.OnFailure(func(p *process.Process) {
+			var serial string
+
+			out, _ := os.ReadFile(p.StdoutPath())
+			err, _ := os.ReadFile(p.StderrPath())
+			status, _ := p.ExitCode()
+
+			if serialBytes, err := os.ReadFile(path.Join(p.StateDir(), "serial.log")); err != nil {
+				serial = fmt.Sprintf("Error reading serial log file: %s\n", err)
+			} else {
+				serial = string(serialBytes)
+			}
+
+			// We are explicitly killing the qemu process. We don't treat that as an error,
+			// but we just print the output just in case.
+			fmt.Printf("\nVM Aborted.\nstdout: %s\nstderr: %s\nserial: %s\nExit status: %s\n", out, err, serial, status)
+			Fail(fmt.Sprintf("\nVM Aborted.\nstdout: %s\nstderr: %s\nserial: %s\nExit status: %s\n",
+				out, err, serial, status))
+		}),
+		types.WithStateDir(stateDir),
+		// Serial output to file: https://superuser.com/a/1412150
+		func(m *types.MachineConfig) error {
+			m.Args = append(m.Args,
+				"-chardev", fmt.Sprintf("stdio,mux=on,id=char0,logfile=%s,signal=off", path.Join(stateDir, "serial.log")),
+				"-serial", "chardev:char0",
+				"-mon", "chardev=char0",
+			)
+			if os.Getenv("EMULATE_TPM") != "" {
+				m.Args = append(m.Args,
+					"-chardev", fmt.Sprintf("socket,id=chrtpm,path=%s/swtpm-sock", path.Join(stateDir, "tpm")),
+					"-tpmdev", "emulator,id=tpm0,chardev=chrtpm", "-device", "tpm-tis,tpmdev=tpm0",
+				)
+			}
+			return nil
+		},
+		// Firmware
+		func(m *types.MachineConfig) error {
+			FW := os.Getenv("FIRMWARE")
+			if FW != "" {
+				getwd, err := os.Getwd()
+				if err != nil {
+					return err
+				}
+				m.Args = append(m.Args, "-drive",
+					fmt.Sprintf("file=%s,if=pflash,format=raw,readonly=on", FW),
+				)
+
+				// Set custom vars file for efi config so we boot first from disk then from DVD with secureboot on
+				UKI := os.Getenv("UKI_TEST")
+				if UKI != "" {
+					// On uki use an empty efivars.fd so we can test the autoenrollment
+					m.Args = append(m.Args, "-drive",
+						fmt.Sprintf("file=%s,if=pflash,format=raw", filepath.Join(getwd, "assets/efivars.empty.fd")),
+					)
+				} else {
+					m.Args = append(m.Args, "-drive",
+						fmt.Sprintf("file=%s,if=pflash,format=raw", filepath.Join(getwd, "assets/efivars.fd")),
+					)
+				}
+				// Needed to be set for secureboot!
+				m.Args = append(m.Args, "-machine", "q35,smm=on")
+			}
+
+			return nil
+		},
+		types.WithDataSource(os.Getenv("DATASOURCE")),
+	}
+	if os.Getenv("KVM") != "" {
+		opts = append(opts, func(m *types.MachineConfig) error {
+			m.Args = append(m.Args,
+				"-enable-kvm",
+			)
+			return nil
+		})
+	}
+
+	if os.Getenv("USE_QEMU") == "true" {
+		opts = append(opts, types.QEMUEngine)
+
+		// You can connect to it with "spicy" or other tool.
+		// DISPLAY is already taken on Linux X sessions
+		if os.Getenv("MACHINE_SPICY") != "" {
+			spicePort, _ = getFreePort()
+			for spicePort == sshPort { // avoid collision
+				spicePort, _ = getFreePort()
+			}
+			display := fmt.Sprintf("-spice port=%d,addr=127.0.0.1,disable-ticketing=yes", spicePort)
+			opts = append(opts, types.WithDisplay(display))
+
+			cmd := exec.Command("spicy",
+				"-h", "127.0.0.1",
+				"-p", strconv.Itoa(spicePort))
+			err = cmd.Start()
+			Expect(err).ToNot(HaveOccurred())
+		}
+	} else {
+		opts = append(opts, types.VBoxEngine)
+	}
+
+	return opts
+}
+
+func HostSSHFingerprint(vm VM) string {
+	By("Getting SSH host key fingerprint")
+	fp, err := vm.Sudo("cat /etc/ssh/ssh_host_*.pub 2>/dev/null | ssh-keygen -lf -")
+	Expect(err).ToNot(HaveOccurred(), fp)
+	Expect(fp).ToNot(BeEmpty(), "SSH host key fingerprint should not be empty")
+	return fp
+}
+
+var stateAssertVM = func(vm VM, query, expected string) {
+	out, err := vm.Sudo(fmt.Sprintf("kairos-agent state get %s", query))
+	ExpectWithOffset(1, err).ToNot(HaveOccurred(), out)
+	ExpectWithOffset(1, out).To(ContainSubstring(expected))
+}
